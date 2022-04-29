0dc1379d2018adf0f1c2038844867e9b7ef4c43f
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2022-04-29 09:18:10 -0400

    formated

--- src/database/entity/Pedido.ts
+++ src/database/entity/Pedido.ts
@@ -2,22 +2,21 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
 export class Pedido {
-    @PrimaryGeneratedColumn()
-    id_pedido!: number
+  @PrimaryGeneratedColumn()
+  id_pedido!: number;
 
-    @Column()
-    id_cliente_pedido!: string
+  @Column()
+  id_cliente_pedido!: string;
 
-    @Column()
-    description_pedido!: string
+  @Column()
+  description_pedido!: string;
 
-    @Column()
-    items_pedido!: string
+  @Column()
+  items_pedido!: string;
 
-    @Column()
-    date_created_pedido!: Date
+  @Column()
+  date_created_pedido!: Date;
 
-    @Column()
-    date_updated_pedido!: Date
-
+  @Column()
+  date_updated_pedido!: Date;
 }
