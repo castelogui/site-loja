7f2853fbba2e5f72381d244e41d1439b501c9d12
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2022-05-04 21:33:02 -0400

    make it better

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
+import compression from 'compression'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(compression(/*  */))
 app.use(routes);
 
 export { app };
