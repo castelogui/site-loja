727762a861be95d26fddae113f23244e6c7339a2
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2022-05-04 21:41:04 -0400

    make it better

--- src/routes.ts
+++ src/routes.ts
@@ -11,7 +11,7 @@ const itemController = new ItemController();
 const routes = express.Router();
 
 routes.post("/", pedidoController.create);
-routes.get("/", )
+routes.get("/", pedidoController.home);
 routes.get("/pedidos", pedidoController.buscar);
 routes.put("/pedidos", pedidoController.update);
 routes.delete("/pedidos", pedidoController.dropPedido);
