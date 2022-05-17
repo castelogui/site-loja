20ea94dbc7f05587d404ef818a80b0633d030de0
Guilherme Castelo <48875867+castelogui@users.noreply.github.com>
2022-05-17 10:35:26 -0400

    Update data-source.ts

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
   host: "localhost",
   port: 5432,
   database: "loja",
-  username: "loja",
+  username: "ubuntu",
   password: "admin",
   applicationName: "api-petiticos",
   synchronize: true,
